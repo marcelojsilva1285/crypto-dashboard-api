8299181618040d90c74346359edc008850f80667
marcelojsilva1285 <marcelo.jsilva1285@gmail.com>
2025-11-16 17:21:54 -0300

    main.js refactoring

--- main.js
+++ main.js
@@ -1,32 +1,23 @@
 import { buscarPreco } from './app.js';
 import { formatString } from './formatting.js';
-import {exibirDados, filtrarLista} from './ui.js'
+import { exibirDados, filtrarLista } from './ui.js';
 
 const inputFilter = document.getElementById('filter-input');
-const cryptoContainer = document.getElementById('crypto-list')
+const cryptoContainer = document.getElementById('crypto-list');
 
 async function atualizarDados() {
   const dados = await buscarPreco();
-
   exibirDados(dados, cryptoContainer);
 }
 
+atualizarDados();
 
 document.getElementById('update-button').addEventListener('click', () => {
   atualizarDados();
 });
 
-async function carregar() {
-  const dados = await buscarPreco()
-  exibirDados(dados, cryptoContainer)
-}
-
-carregar()
-
-
 inputFilter.addEventListener('input', (e) => {
   const inputValue = formatString(e.target.value);
- 
-  filtrarLista(inputValue)
 
+  filtrarLista(inputValue);
 });
